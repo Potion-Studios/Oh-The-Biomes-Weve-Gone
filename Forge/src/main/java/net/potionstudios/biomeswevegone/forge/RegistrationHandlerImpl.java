ae3da47c790c4c92719ed28b5d6905d8bee64d3c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-07-30 17:38:54 -0400

    Documentation

--- Forge/src/main/java/net/potionstudios/biomeswevegone/forge/RegistrationHandlerImpl.java
+++ Forge/src/main/java/net/potionstudios/biomeswevegone/forge/RegistrationHandlerImpl.java
@@ -2,7 +2,6 @@ package net.potionstudios.biomeswevegone.forge;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,12 +20,6 @@ import java.util.function.Supplier;
  */
 public class RegistrationHandlerImpl {
 
-    private static final DeferredRegister<Codec<? extends SurfaceRules.RuleSource>> MATERIAL_RULES = DeferredRegister.create(Registries.MATERIAL_RULE, BiomesWeveGone.MOD_ID);
-
-    public static Supplier<Codec<? extends SurfaceRules.RuleSource>> registerMaterialRule(String id, Supplier<Codec<? extends SurfaceRules.RuleSource>> codec) {
-        return MATERIAL_RULES.register(id, codec);
-    }
-
     private static final DeferredRegister<BlockPredicateType<?>> BLOCK_PREDICATE_TYPE = DeferredRegister.create(Registries.BLOCK_PREDICATE_TYPE, BiomesWeveGone.MOD_ID);
 
     public static <P extends BlockPredicate> Supplier<BlockPredicateType<P>> registerBlockPredicate(String id, Supplier<Codec<P>> codec) {
@@ -34,8 +27,6 @@ public class RegistrationHandlerImpl {
     }
 
     public static void init(IEventBus bus) {
-        MATERIAL_RULES.register(bus);
         BLOCK_PREDICATE_TYPE.register(bus);
     }
-
 }
