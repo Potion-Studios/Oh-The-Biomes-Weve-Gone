6716cae97e3f846cf56b286f656aa466b002d5f9
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-08-23 17:21:32 -0400

    Switch the way the list is to make sense

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/npc/BWGVillagerTrades.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/npc/BWGVillagerTrades.java
@@ -1,15 +1,15 @@
 package net.potionstudios.biomeswevegone.world.entity.npc;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.trading.MerchantOffer;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 public class BWGVillagerTrades {
 
-    public static final Map<BiConsumer<VillagerProfession, Integer>, MerchantOffer> TRADES = new HashMap<>();
+    public static final Map<VillagerProfession, Pair<Integer, MerchantOffer>> TRADES = new HashMap<>();
 
     public static void makeTrades() {
 
