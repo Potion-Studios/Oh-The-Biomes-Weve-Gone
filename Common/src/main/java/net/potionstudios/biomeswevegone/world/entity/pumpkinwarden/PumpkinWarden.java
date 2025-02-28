a8237cb222c6041e6e4b6ef899a33955d7a591c3
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-02-28 14:53:25 -0500

    Fix PumpkinWarden Look around goals not being Registered

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/pumpkinwarden/PumpkinWarden.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/pumpkinwarden/PumpkinWarden.java
@@ -74,10 +74,8 @@ public class PumpkinWarden extends PathfinderMob implements GeoEntity, VariantHo
     private static final EntityDataAccessor<BlockState> DATA_CARRY_STATE = SynchedEntityData.defineId(PumpkinWarden.class, EntityDataSerializers.BLOCK_STATE);
     private static final EntityDataAccessor<Integer> DATA_VARIANT = SynchedEntityData.defineId(PumpkinWarden.class, EntityDataSerializers.INT);
 
-    public Goal moveGoal = new WaterAvoidingRandomStrollGoal(this, 1.0D, 0.7F);
-    public Goal runGoal = new AvoidEntityGoal<>(this, Zombie.class, 8.0F, 1.0D, 1.0D);
-    public Goal lookGoal = new LookAtPlayerGoal(this, Player.class, 2.0F);
-    public Goal randLookGoal = new RandomLookAroundGoal(this);
+    private Goal moveGoal = new WaterAvoidingRandomStrollGoal(this, 1.0D, 0.7F);
+    private Goal runGoal = new AvoidEntityGoal<>(this, Zombie.class, 8.0F, 1.0D, 1.0D);
 
     public PumpkinWarden(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
@@ -110,12 +108,14 @@ public class PumpkinWarden extends PathfinderMob implements GeoEntity, VariantHo
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(9, new FloatGoal(this));
-        this.goalSelector.addGoal(4, new TemptGoal(this, 1.2D, Ingredient.of(Items.PUMPKIN_PIE), false));
-        this.goalSelector.addGoal(2, new ThrowItemAtCarvedPumpkinGoal(this, 1));
-        this.goalSelector.addGoal(1, new DestroyNearestPumpkinGoal(this, 1));
-        this.goalSelector.addGoal(5, new StayByBellGoal(this, 1, 5000));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        goalSelector.addGoal(0, new FloatGoal(this));
+        targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        goalSelector.addGoal(1, new DestroyNearestPumpkinGoal(this, 1));
+        goalSelector.addGoal(2, new ThrowItemAtCarvedPumpkinGoal(this, 1));
+        goalSelector.addGoal(4, new TemptGoal(this, 1.2D, Ingredient.of(Items.PUMPKIN_PIE), false));
+        goalSelector.addGoal(5, new StayByBellGoal(this, 1, 5000));
+        goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        goalSelector.addGoal(8, new RandomLookAroundGoal(this));
     }
 
     @Override
@@ -208,10 +208,10 @@ public class PumpkinWarden extends PathfinderMob implements GeoEntity, VariantHo
             this.goalSelector.addGoal(2, runGoal);
         }
         if (this.goalSelector.getAvailableGoals().stream().noneMatch(goal -> goal.getGoal().getClass() == LookAtPlayerGoal.class)) {
-            this.goalSelector.addGoal(7, lookGoal);
+            this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 8.0F));
         }
         if (this.goalSelector.getAvailableGoals().stream().noneMatch(goal -> goal.getGoal().getClass() == RandomLookAroundGoal.class)) {
-            this.goalSelector.addGoal(3, randLookGoal);
+            this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
         }
     }
 
@@ -249,8 +249,14 @@ public class PumpkinWarden extends PathfinderMob implements GeoEntity, VariantHo
         if (this.isHiding()) {
             this.goalSelector.removeGoal(moveGoal);
             this.goalSelector.removeGoal(runGoal);
-            this.goalSelector.removeGoal(lookGoal);
-            this.goalSelector.removeGoal(randLookGoal);
+            goalSelector.getAvailableGoals().forEach(goal -> {
+                if (goal.getGoal().getClass() == WaterAvoidingRandomStrollGoal.class) {
+                    this.goalSelector.removeGoal(goal.getGoal());
+                }
+                if (goal.getGoal().getClass() == AvoidEntityGoal.class) {
+                    this.goalSelector.removeGoal(goal.getGoal());
+                }
+            });
             if (this.getCarriedBlock() != null) {
                 BehaviorUtils.throwItem(this, this.getCarriedBlock().getBlock().asItem().getDefaultInstance(), new Vec3(this.getX() + 2, this.getY(), this.getZ()));
                 this.setCarriedBlock(null);
