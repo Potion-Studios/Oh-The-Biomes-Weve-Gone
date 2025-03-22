c7116373f2523dfc6082ba1d996abdf916489ab9
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-21 22:16:31 -0400

    BackPort: Remove Pointless TagKey param from FlatVegetationBlock

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/vegetation/FlatVegetationBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/vegetation/FlatVegetationBlock.java
@@ -1,7 +1,6 @@
 package net.potionstudios.biomeswevegone.world.level.block.plants.vegetation;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BushBlock;
@@ -11,34 +10,17 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class FlatVegetationBlock extends BushBlock {
 
     private static final VoxelShape VOXEL_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1D, 16.0D);
 
-    private final @Nullable TagKey<Block> validGround;
-
-    public FlatVegetationBlock(Properties properties, @Nullable TagKey<Block> validGround) {
-        super(properties);
-        this.validGround = validGround;
-    }
-
-    public FlatVegetationBlock(Properties properties) {
-        this(properties, null);
-    }
-
     public FlatVegetationBlock() {
-        this(BlockBehaviour.Properties.of().noCollission().instabreak().sound(SoundType.GRASS));
+        super(BlockBehaviour.Properties.of().noCollission().instabreak().sound(SoundType.GRASS));
     }
 
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
         return VOXEL_SHAPE;
-    }
-
-    @Override
-    protected boolean mayPlaceOn(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos) {
-        return validGround == null ? super.mayPlaceOn(state, level, pos) : state.is(validGround);
     }
 }
