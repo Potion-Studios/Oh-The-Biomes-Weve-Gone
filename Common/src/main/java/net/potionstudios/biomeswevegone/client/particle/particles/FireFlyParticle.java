763bd6a0df4ce0bee9662f456b0b636fce59e95b
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-22 00:48:34 -0400

    Make Particle providers records
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/client/particle/particles/FireFlyParticle.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/client/particle/particles/FireFlyParticle.java
@@ -4,17 +4,9 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class FireFlyParticle extends TextureSheetParticle {
-	protected FireFlyParticle(ClientLevel level, double x, double y, double z) {
-		super(level, x, y, z);
-		this.gravity = 0.0F;
-		this.lifetime = this.random.nextInt(250, 500);
-		this.setSize(0.01F, 0.01F);
-	}
-
-	protected FireFlyParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+	FireFlyParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
 		super(level, x, y, z, xSpeed, ySpeed, zSpeed);
 		this.gravity = 0.0F;
 		this.lifetime = this.random.nextInt(250, 500);
@@ -44,15 +36,7 @@ public class FireFlyParticle extends TextureSheetParticle {
 		}
 	}
 
-	public static class Provider implements ParticleProvider<SimpleParticleType> {
-
-		private final SpriteSet sprite;
-
-		public Provider(SpriteSet sprite) {
-			this.sprite = sprite;
-		}
-
-		@Nullable
+	public record Provider(SpriteSet sprite) implements ParticleProvider<SimpleParticleType> {
 		@Override
 		public Particle createParticle(@NotNull SimpleParticleType var1, @NotNull ClientLevel world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
 			FireFlyParticle fireFlyParticle = new FireFlyParticle(world, x, y, z, xSpeed, ySpeed, zSpeed);
