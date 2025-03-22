4dc978a17a9f6ad0926139226b2ffd7ef6dfc49b
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-21 22:05:05 -0400

    BackPort: Remove Pointless TagKey param from FlatVegetationBlock

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/vegetation/FlatVegetationBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/vegetation/FlatVegetationBlock.java
@@ -2,7 +2,6 @@ package net.potionstudios.biomeswevegone.world.level.block.plants.vegetation;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BushBlock;
@@ -12,27 +11,15 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class FlatVegetationBlock extends BushBlock {
 
     private static final VoxelShape VOXEL_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1D, 16.0D);
 
-    private final @Nullable TagKey<Block> validGround;
     private final MapCodec<FlatVegetationBlock> CODEC = simpleCodec(FlatVegetationBlock::new);
 
-    public FlatVegetationBlock(Properties properties, @Nullable TagKey<Block> validGround) {
-        super(properties);
-        this.validGround = validGround;
-    }
-
     public FlatVegetationBlock(Properties properties) {
-        this(properties, null);
-    }
-
-    @Override
-    protected @NotNull MapCodec<? extends BushBlock> codec() {
-        return CODEC;
+        super(properties);
     }
 
     public FlatVegetationBlock() {
@@ -40,12 +27,12 @@ public class FlatVegetationBlock extends BushBlock {
     }
 
     @Override
-    public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        return VOXEL_SHAPE;
+    protected @NotNull MapCodec<? extends BushBlock> codec() {
+        return CODEC;
     }
 
     @Override
-    protected boolean mayPlaceOn(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos) {
-        return validGround == null ? super.mayPlaceOn(state, level, pos) : state.is(validGround);
+    public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return VOXEL_SHAPE;
     }
 }
