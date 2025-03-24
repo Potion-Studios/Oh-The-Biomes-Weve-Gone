e8c6256294ea4cfa4025216ab7aaa547d0a6896d
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-24 14:00:39 -0400

    Rename White Dacite Bricks Variants
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/structure/processor/BWGRuleProcessors.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/structure/processor/BWGRuleProcessors.java
@@ -163,8 +163,8 @@ class BWGRuleProcessors {
 	protected static final RuleProcessor WHITE_DACITE_VARIANTS = createRuleProcessor(
 			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.3f, BWGBlocks.WHITE_DACITE_COBBLESTONE_SET.getBase()),
 			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.3f, BWGBlocks.WHITE_DACITE_BRICKS_SET.getBase()),
-			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.1f, BWGBlocks.WHITE_DACITE_CRACKED_BRICKS_SET.getBase()),
-			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.1f, BWGBlocks.WHITE_DACITE_MOSSY_BRICKS_SET.getBase())
+			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.1f, BWGBlocks.CRACKED_WHITE_DACITE_BRICKS_SET.getBase()),
+			createAlwaysTrueRandomBlockMatchTest(BWGBlocks.WHITE_DACITE_SET.getBase(), 0.1f, BWGBlocks.MOSSY_WHITE_DACITE_BRICKS_SET.getBase())
 	);
 
 	protected static final RuleProcessor STONEBRICKS_VARIANTS = createRuleProcessor(
