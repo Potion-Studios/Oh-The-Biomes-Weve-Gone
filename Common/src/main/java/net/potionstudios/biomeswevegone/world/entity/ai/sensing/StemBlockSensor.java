9573bb563d48b31af9a2ed4bdb0ba5a0443421c0
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-29 14:21:17 -0400

    StemBlockSensor

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/sensing/StemBlockSensor.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/sensing/StemBlockSensor.java
@@ -5,21 +5,31 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.sensing.Sensor;
+import net.minecraft.world.level.block.AttachedStemBlock;
+import net.minecraft.world.level.block.StemBlock;
 import net.potionstudios.biomeswevegone.world.entity.ai.memory.BWGMemoryModuleType;
 import net.potionstudios.biomeswevegone.world.entity.pumpkinwarden.PumpkinWarden;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class StemBlockSensor extends Sensor<PumpkinWarden> {
     @Override
     protected void doTick(@NotNull ServerLevel level, @NotNull PumpkinWarden entity) {
-        entity.getBrain().setMemory(BWGMemoryModuleType.PUMPKIN_STEMS.get(), getStemPositions(entity));
-    }
-
-    private List<BlockPos> getStemPositions(PumpkinWarden entity) {
-        return entity.getBrain().getMemory(BWGMemoryModuleType.PUMPKIN_STEMS.get()).orElse(List.of());
+        List<BlockPos> stemPositions = new ArrayList<>();
+        BlockPos blockPos = entity.blockPosition();
+        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+        for (int y = 2; -2 <= y; y--)
+            for (int x = -25; x < 25; x++)
+                for (int z = -25; z < 25; z++) {
+                    mutableBlockPos.setWithOffset(blockPos, x, y, z);
+                    if (level.getBlockState(mutableBlockPos).getBlock() instanceof AttachedStemBlock
+                            || level.getBlockState(mutableBlockPos).getBlock() instanceof StemBlock)
+                        stemPositions.add(mutableBlockPos.immutable());
+                }
+        entity.getBrain().setMemory(BWGMemoryModuleType.PUMPKIN_STEMS.get(), stemPositions);
     }
 
     @Override
