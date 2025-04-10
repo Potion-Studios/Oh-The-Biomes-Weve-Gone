458ab099ae12ec0dab53ae86e48a74b202d6b535
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-04-09 20:15:59 -0400

    They harvest them correctly now

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/behavior/DestroyPumpkin.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/behavior/DestroyPumpkin.java
@@ -5,7 +5,12 @@ import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.behavior.BlockPosTracker;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
+import net.minecraft.world.entity.ai.memory.WalkTarget;
+import net.minecraft.world.level.block.AttachedStemBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.potionstudios.biomeswevegone.BiomesWeveGone;
@@ -13,50 +18,65 @@ import net.potionstudios.biomeswevegone.world.entity.ai.memory.BWGMemoryModuleTy
 import net.potionstudios.biomeswevegone.world.entity.pumpkinwarden.PumpkinWarden;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class DestroyPumpkin extends Behavior<PumpkinWarden> {
 
-    private BlockPos targetPos = BlockPos.ZERO;
+    private BlockPos targetBlock;
     protected int tryTicks;
 
     public DestroyPumpkin() {
-        super(Util.make(() -> ImmutableMap.of(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get(), MemoryStatus.VALUE_PRESENT)));
+        super(Util.make(() -> ImmutableMap.of(
+                BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get(), MemoryStatus.VALUE_PRESENT,
+                MemoryModuleType.WALK_TARGET, MemoryStatus.REGISTERED,
+                MemoryModuleType.LOOK_TARGET, MemoryStatus.REGISTERED)));
     }
 
     @Override
     protected boolean checkExtraStartConditions(@NotNull ServerLevel level, @NotNull PumpkinWarden entity) {
-        BiomesWeveGone.LOGGER.info("Checking if Pumpkin has been destroyed.");
-        return entity.canMove() && entity.getCarriedBlock() == null;
+        return entity.canMove() && entity.getCarriedBlock() == null && entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).isPresent() && !entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).get().isEmpty();
     }
 
     @Override
     protected void tick(@NotNull ServerLevel level, @NotNull PumpkinWarden entity, long gameTime) {
-        if (!targetPos.closerToCenterThan(entity.position(), 1.0)) {
+        if (targetBlock == null) return;
+        BiomesWeveGone.LOGGER.info("Destroying pumpkin stem at {}", targetBlock);
+        if (targetBlock.closerToCenterThan(entity.position(), 1.0)) {
             tryTicks++;
-            if (tryTicks % 40 == 0)
-                entity.getNavigation().moveTo(targetPos.getX() + 0.5, targetPos.getY(), targetPos.getZ() + 0.5, 1.0);
-        } else {
-            BlockState blockState = level.getBlockState(targetPos);
-            level.destroyBlock(targetPos, false, entity);
-            level.gameEvent(GameEvent.BLOCK_DESTROY, targetPos, GameEvent.Context.of(entity, blockState));
+        } else if (!level.getBlockState(targetBlock).isAir()){
+            BlockState blockState = level.getBlockState(targetBlock);
+            level.destroyBlock(targetBlock, false, entity);
+            level.gameEvent(GameEvent.BLOCK_DESTROY, targetBlock, GameEvent.Context.of(entity, blockState));
             entity.setCarriedBlock(blockState);
+            stop(level, entity, gameTime);
         }
     }
 
     @Override
     protected boolean canStillUse(@NotNull ServerLevel level, @NotNull PumpkinWarden entity, long gameTime) {
-        return entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).isPresent() && entity.getCarriedBlock() != null && entity.canMove();
+        return entity.getCarriedBlock() == null && entity.canMove() && entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).isPresent();
     }
 
     @Override
     protected void start(@NotNull ServerLevel level, @NotNull PumpkinWarden entity, long gameTime) {
-        if (entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).isPresent()) {
-            BiomesWeveGone.LOGGER.info("StemBlocks: {}", entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).get());
-        }
-        /*
-        entity.getBrain().getMemory(BWGMemoryModuleType.PUMPKIN_STEMS.get()).ifPresent(stemPositions ->
-                targetPos = stemPositions.get(entity.getRandom().nextInt(stemPositions.size())));
-        tryTicks = 0;
+        List<BlockPos> blockPosList = entity.getBrain().getMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get()).get();
+        BiomesWeveGone.LOGGER.info("Found {} visible pumpkin stems", blockPosList.size());
+        BlockPos blockPos = blockPosList.get(level.getRandom().nextInt(blockPosList.size()));
+        Block block =  level.getBlockState(blockPos).getBlock();
+        BiomesWeveGone.LOGGER.info("Stem Block is {}", block.builtInRegistryHolder());
+        if (block instanceof AttachedStemBlock) {
+            BiomesWeveGone.LOGGER.info("Found pumpkin stem at {}", blockPos);
+            targetBlock = blockPos.relative(level.getBlockState(blockPos).getValue(AttachedStemBlock.FACING));
+            BiomesWeveGone.LOGGER.info("Target stem block is {}", targetBlock);
+            entity.getBrain().setMemory(MemoryModuleType.WALK_TARGET, new WalkTarget(targetBlock, 1.0F, 0));
+            entity.getBrain().setMemory(MemoryModuleType.LOOK_TARGET, new BlockPosTracker(targetBlock));
+            BiomesWeveGone.LOGGER.info("Memories are set, Walk target is {}, Look target is {}", entity.getBrain().getMemory(MemoryModuleType.WALK_TARGET), entity.getBrain().getMemory(MemoryModuleType.LOOK_TARGET));
+        } else stop(level, entity, gameTime);
+    }
 
-         */
+    @Override
+    protected void stop(@NotNull ServerLevel level, @NotNull PumpkinWarden entity, long gameTime) {
+        entity.getBrain().eraseMemory(MemoryModuleType.LOOK_TARGET);
+        entity.getBrain().eraseMemory(MemoryModuleType.WALK_TARGET);
     }
 }
--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/sensing/StemBlockSensor.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/ai/sensing/StemBlockSensor.java
@@ -6,8 +6,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.level.block.AttachedStemBlock;
-import net.minecraft.world.level.block.StemBlock;
-import net.minecraft.world.level.block.state.BlockState;
+import net.potionstudios.biomeswevegone.BiomesWeveGone;
 import net.potionstudios.biomeswevegone.world.entity.ai.memory.BWGMemoryModuleType;
 import net.potionstudios.biomeswevegone.world.entity.pumpkinwarden.PumpkinWarden;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +18,7 @@ import java.util.Set;
 public class StemBlockSensor extends Sensor<PumpkinWarden> {
     @Override
     protected void doTick(@NotNull ServerLevel level, @NotNull PumpkinWarden entity) {
+        BiomesWeveGone.LOGGER.debug("Checking for pumpkin stems");
         List<BlockPos> stemPositions = new ArrayList<>();
         BlockPos blockPos = entity.blockPosition();
         BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
@@ -26,11 +26,8 @@ public class StemBlockSensor extends Sensor<PumpkinWarden> {
             for (int x = -25; x < 25; x++)
                 for (int z = -25; z < 25; z++) {
                     mutableBlockPos.setWithOffset(blockPos, x, y, z);
-                    BlockState state = level.getBlockState(mutableBlockPos);
-                    if (state.getBlock() instanceof AttachedStemBlock)
-                        stemPositions.add(mutableBlockPos.relative(state.getValue(AttachedStemBlock.FACING)));
-                    else if (state.getBlock() instanceof StemBlock && state.getValue(StemBlock.AGE) == StemBlock.MAX_AGE)
-                        stemPositions.add(mutableBlockPos);
+                    if (level.getBlockState(mutableBlockPos).getBlock() instanceof AttachedStemBlock)
+                        stemPositions.add(mutableBlockPos.immutable());
                 }
         if (stemPositions.isEmpty()) entity.getBrain().eraseMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get());
         else entity.getBrain().setMemory(BWGMemoryModuleType.VISIBLE_PUMPKIN_STEMS.get(), stemPositions);
