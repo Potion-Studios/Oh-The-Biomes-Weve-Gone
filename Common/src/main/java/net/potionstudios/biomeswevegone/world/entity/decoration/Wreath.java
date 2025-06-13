df62abd56250a939a43f4ee61dbb2158ecae26e7
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-06-13 15:37:36 -0400

    Allow Placing Wreaths on top and bottom of blocks

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/decoration/Wreath.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/decoration/Wreath.java
@@ -21,7 +21,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DiodeBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.AABB;
 import net.potionstudios.biomeswevegone.world.entity.BWGEntityType;
 import net.potionstudios.biomeswevegone.world.item.BWGItems;
 import net.potionstudios.biomeswevegone.world.item.custom.WreathItem;
@@ -65,6 +68,45 @@ public class Wreath extends HangingEntity implements VariantHolder<Wreath.Type> 
         this.xRotO = this.getXRot();
         this.yRotO = this.getYRot();
         this.recalculateBoundingBox();
+    }
+
+    @Override
+    protected void recalculateBoundingBox() {
+        if (this.direction != null) {
+            double e = this.pos.getX() + 0.5 - this.direction.getStepX() * 0.46875;
+            double f = this.pos.getY() + 0.5 - this.direction.getStepY() * 0.46875;
+            double g = this.pos.getZ() + 0.5 - this.direction.getStepZ() * 0.46875;
+            this.setPosRaw(e, f, g);
+            double h = this.getWidth();
+            double i = this.getHeight();
+            double j = this.getWidth();
+            Direction.Axis axis = this.direction.getAxis();
+            switch (axis) {
+                case X:
+                    h = 1.0;
+                    break;
+                case Y:
+                    i = 1.0;
+                    break;
+                case Z:
+                    j = 1.0;
+            }
+
+            h /= 32.0;
+            i /= 32.0;
+            j /= 32.0;
+            this.setBoundingBox(new AABB(e - h, f - i, g - j, e + h, f + i, g + j));
+        }
+    }
+
+    @Override
+    public boolean survives() {
+        if (!this.level().noCollision(this)) {
+            return false;
+        } else {
+            BlockState blockState = this.level().getBlockState(this.pos.relative(this.direction.getOpposite()));
+            return (blockState.isSolid() || this.direction.getAxis().isHorizontal() && DiodeBlock.isDiode(blockState)) && this.level().getEntities(this, this.getBoundingBox(), HANGING_ENTITY).isEmpty();
+        }
     }
 
     @Override
