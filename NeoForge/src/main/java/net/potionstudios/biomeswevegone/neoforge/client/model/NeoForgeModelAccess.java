2cb4b0e25c309994d23e0a16dce643cd49fc1591
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-06-19 09:36:23 -0400

    Yay the Wreath Renders on NeoForge now
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/client/model/NeoForgeModelAccess.java
+++ NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/client/model/NeoForgeModelAccess.java
@@ -1,4 +1,4 @@
-package net.potionstudios.biomeswevegone.fabric.client.model;
+package net.potionstudios.biomeswevegone.neoforge.client.model;
 
 import com.google.auto.service.AutoService;
 import net.minecraft.client.Minecraft;
@@ -7,9 +7,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.potionstudios.biomeswevegone.client.model.ModelAccess;
 
 @AutoService(ModelAccess.class)
-public final class FabricModelAccess implements ModelAccess {
+public final class NeoForgeModelAccess implements ModelAccess {
 	@Override
 	public BakedModel getModel(ResourceLocation location) {
-		return Minecraft.getInstance().getModelManager().getModel(location);
+		return Minecraft.getInstance().getModelManager().getStandaloneModel(location);
 	}
 }
