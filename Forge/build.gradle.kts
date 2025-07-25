900f408ab4fab8f9e73a934d90500c65c13d5c8c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-07-24 21:13:40 -0400

    Update Corgilib Maven Path and Version

--- Forge/build.gradle.kts
+++ Forge/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     modLocalRuntime("me.djtheredstoner:DevAuth-forge-latest:${project.properties["devauth_version"]}")
 
     modApi("com.github.glitchfiend:TerraBlender-forge:$minecraftVersion-${project.properties["terrablender_version"]}")
-    modApi("corgitaco.corgilib:Corgilib-Forge:$minecraftVersion-${project.properties["corgilib_version"]}")
+    modApi("dev.corgitaco:Corgilib-Forge:$minecraftVersion-${project.properties["corgilib_version"]}")
     modApi("dev.corgitaco:Oh-The-Trees-Youll-Grow-forge:$minecraftVersion-${project.properties["ohthetreesyoullgrow_version"]}")
     modApi("software.bernie.geckolib:geckolib-forge-$minecraftVersion:${project.properties["geckolib_version"]}")
     compileOnly("net.luckperms:api:5.4")
