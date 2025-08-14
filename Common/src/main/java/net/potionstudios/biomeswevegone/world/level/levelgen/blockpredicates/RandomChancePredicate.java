ccf69e15bcf130a39c59b4ab16e55f5aa83d5912
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-08-13 21:51:59 -0400

    Move somethings to records
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/blockpredicates/RandomChancePredicate.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/blockpredicates/RandomChancePredicate.java
@@ -10,28 +10,22 @@ import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
 import org.jetbrains.annotations.NotNull;
 
-public class RandomChancePredicate implements BlockPredicate {
+public record RandomChancePredicate(FloatProvider chance) implements BlockPredicate {
 
-    public static final MapCodec<RandomChancePredicate> CODEC = RecordCodecBuilder.mapCodec(instance ->
-            instance.group(
-                    FloatProvider.CODEC.fieldOf("chance").forGetter(randomChancePredicate -> randomChancePredicate.chance)
-            ).apply(instance, RandomChancePredicate::new)
-    );
+	public static final MapCodec<RandomChancePredicate> CODEC = RecordCodecBuilder.mapCodec(instance ->
+			instance.group(
+					FloatProvider.CODEC.fieldOf("chance").forGetter(randomChancePredicate -> randomChancePredicate.chance)
+			).apply(instance, RandomChancePredicate::new)
+	);
 
-    private final FloatProvider chance;
+	@Override
+	public @NotNull BlockPredicateType<?> type() {
+		return BWGBlockPredicateTypes.RANDOM_CHANCE.get();
+	}
 
-    public RandomChancePredicate(FloatProvider chance) {
-        this.chance = chance;
-    }
-
-    @Override
-    public @NotNull BlockPredicateType<?> type() {
-        return BWGBlockPredicateTypes.RANDOM_CHANCE.get();
-    }
-
-    @Override
-    public boolean test(WorldGenLevel level, BlockPos blockPos) {
-        XoroshiroRandomSource xoroshiroRandomSource = new XoroshiroRandomSource(blockPos.asLong() + level.getSeed());
-        return xoroshiroRandomSource.nextDouble() < this.chance.sample(xoroshiroRandomSource);
-    }
+	@Override
+	public boolean test(WorldGenLevel level, BlockPos blockPos) {
+		XoroshiroRandomSource xoroshiroRandomSource = new XoroshiroRandomSource(blockPos.asLong() + level.getSeed());
+		return xoroshiroRandomSource.nextDouble() < this.chance.sample(xoroshiroRandomSource);
+	}
 }
