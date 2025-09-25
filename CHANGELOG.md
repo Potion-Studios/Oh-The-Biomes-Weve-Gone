13415cf610bbde9407ce6b4a3ac40b05ac60da8c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-09-24 22:48:16 -0400

    Add some Missing block and item tags

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # 2.4.8
 - Fix missing Chicken Pool and Missing Pool warnings in Pumpkin Patch Village
+- Add some Missing Block and Item Tags
 
 # 2.4.7
 - Add Pale Pumpkin Seeds to Chicken and Parrot Food
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.11-SNAPSHOT" apply false
-    id("com.gradleup.shadow") version "9.1.0" apply false
-    id("com.hypherionmc.modutils.modpublisher") version "2.1.6"
+    id("com.gradleup.shadow") version "9.2.1" apply false
+    id("com.hypherionmc.modutils.modpublisher") version "2.+"
     java
     idea
     `maven-publish`
