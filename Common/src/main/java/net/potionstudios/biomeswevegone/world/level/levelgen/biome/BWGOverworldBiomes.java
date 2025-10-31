c3d35d46c47c2a91f465a5773d0238aff465408c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-10-31 15:34:11 -0400

    Remove some unneeded Vegetation

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/biome/BWGOverworldBiomes.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/biome/BWGOverworldBiomes.java
@@ -200,8 +200,6 @@ class BWGOverworldBiomes {
     protected static Biome basaltBarrera(HolderGetter<PlacedFeature> placedFeatureGetter, HolderGetter<ConfiguredWorldCarver<?>> carverGetter) {
         BiomeGenerationSettings.Builder generationSettings = setupDefaultOverworldGeneration(placedFeatureGetter, carverGetter);
 
-        BiomeDefaultFeatures.addDefaultGrass(generationSettings);
-
         addRawGeneration(generationSettings, BWGPlacedFeatures.LARGE_BASALT_COLUMN);
         addRawGeneration(generationSettings, BWGPlacedFeatures.SMALL_BASALT_COLUMN);
         addRawGeneration(generationSettings, BWGPlacedFeatures.BASALT_DELTA);
@@ -581,11 +579,6 @@ class BWGOverworldBiomes {
 
     protected static Biome daciteShore(HolderGetter<PlacedFeature> placedFeatureGetter, HolderGetter<ConfiguredWorldCarver<?>> carverGetter) {
         BiomeGenerationSettings.Builder generationSettings = setupDefaultOverworldGeneration(placedFeatureGetter, carverGetter);
-        BiomeDefaultFeatures.addFerns(generationSettings);
-
-        BiomeDefaultFeatures.addDefaultFlowers(generationSettings);
-        BiomeDefaultFeatures.addJungleGrass(generationSettings);
-        BiomeDefaultFeatures.addDefaultMushrooms(generationSettings);
 
         BWGOverworldDefaultFeatures.addRockyStoneBoulder(generationSettings);
 
@@ -630,7 +623,6 @@ class BWGOverworldBiomes {
         BiomeGenerationSettings.Builder generationSettings = new BiomeGenerationSettings.Builder(placedFeatureGetter, carverGetter);
         BiomeDefaultFeatures.addFossilDecoration(generationSettings);
         addDefaultOverworldGeneration(generationSettings);
-        BiomeDefaultFeatures.addDefaultFlowers(generationSettings);
         BiomeDefaultFeatures.addDesertVegetation(generationSettings);
         addRawGeneration(generationSettings, BWGPlacedFeatures.WINDSWEPT_BOULDER);
         addVegetal(generationSettings, BWGOverworldVegationPlacedFeatures.WINDSWEPT_DESERT_VEGETATION);
