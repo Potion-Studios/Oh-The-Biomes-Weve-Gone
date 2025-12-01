9df671c2c8c29afc2c5d719384dea6184abfc5bb
AOCAWOL <biomesyougo@gmail.com>
2025-12-01 15:11:43 -0700

    -added frosted fir assets -added frost amber assets -updated fir leaves

--- Common/src/main/java/net/potionstudios/biomeswevegone/config/configs/BWGWorldGenConfig.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/config/configs/BWGWorldGenConfig.java
@@ -47,6 +47,8 @@ public record BWGWorldGenConfig(Map<ResourceKey<Biome>, Boolean> enabledBiomes, 
         for (ResourceKey<Biome> biomeResourceKey : BWGBiomes.BIOME_FACTORIES.keySet()) {
             enabledBiomes.put(biomeResourceKey, true);
         }
+
+        enabledBiomes.replace(BWGBiomes.ERODED_BOREALIS, true);
         return enabledBiomes;
     }
 
