9a2e2ebfa980875d3ed4373c5db82139dede13c1
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-12-03 20:56:56 -0500

    Frosted Fir Set
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/datagen/DataGeneratorsRegister.java
+++ NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/datagen/DataGeneratorsRegister.java
@@ -54,7 +54,6 @@ class DataGeneratorsRegister {
         generator.addProvider(event.includeServer(), datapackBuiltinEntriesProvider);
         lookupProvider = datapackBuiltinEntriesProvider.getRegistryProvider();
 
-        ModelGenerators.init(generator, event.includeClient(), output, existingFileHelper);
         generator.addProvider(event.includeClient(), new LangGenerator(output, "en_us"));
         generator.addProvider(event.includeServer(), new RecipeGenerator(output, lookupProvider));
         generator.addProvider(event.includeServer(), new LootGenerator(output, lookupProvider));
@@ -65,6 +64,7 @@ class DataGeneratorsRegister {
         generator.addProvider(event.includeClient(), new ParticleDescriptionGenerator(output, existingFileHelper));
         generator.addProvider(event.includeClient(), new SoundDefinitionsGenerator(output, existingFileHelper));
         generator.addProvider(event.includeServer(), new DatamapGenerator(output, lookupProvider));
+        ModelGenerators.init(generator, event.includeClient(), output, existingFileHelper);
     }
 
     private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
