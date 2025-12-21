0b713dce7f37e169525afb86359302739346f678
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-12-20 23:48:23 -0500

    Add PathBlockType to BlueBerry Bush (Forge/NeoForge Only) Closes #347
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # 4.2.1
 - Fix Baby Villagers not being able to be turned into Pumpkin Wardens (Fabric)
 - Adjust some Language Keys for Skyris Vale Biome
+- Add PathBlockType to BlueBerry Bush.  (Forge/NeoForge Only)
 
 # 4.2.0 - 1.21.10
 - Update to 1.21.10, Support for 1.21.8 has now ended
--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/bush/BWGBerryBush.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/bush/BWGBerryBush.java
@@ -6,16 +6,20 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.pathfinder.PathType;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
@@ -53,5 +57,15 @@ public class BWGBerryBush extends SweetBerryBushBlock {
     @Override
     protected void entityInside(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Entity entity, @NotNull InsideBlockEffectApplier applier, boolean intersects) {
         if (hurtEntityInside) super.entityInside(state, level, pos, entity, applier, intersects);
+    }
+
+    /**
+     * Overrides the path type for mobs walking through the bush.getBlockPathType
+     * @see net.neoforged.neoforge.common.extensions.IBlockExtension#getBlockPathType
+     * @see net.minecraftforge.common.extensions.IForgeBlock#getBlockPathType
+     */
+    @Nullable
+    PathType getBlockPathType(BlockState state, BlockGetter level, BlockPos pos, @Nullable Mob mob) {
+        return hurtEntityInside ? PathType.DAMAGE_OTHER : null;
     }
 }
