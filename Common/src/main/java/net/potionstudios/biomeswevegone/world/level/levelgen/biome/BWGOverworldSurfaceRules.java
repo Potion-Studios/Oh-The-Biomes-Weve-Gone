13024d6dfb61c993ea1ef9a1c94345c4272afe0e
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-02-28 10:09:28 -0500

    Adjust Windswept Desert Surface Rules to Spawn more layers of Sand, and fix Grass Spawning instead
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/biome/BWGOverworldSurfaceRules.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/biome/BWGOverworldSurfaceRules.java
@@ -32,11 +32,11 @@ public class BWGOverworldSurfaceRules {
     private static final SurfaceRules.RuleSource GRAVEL = SurfaceRules.sequence(makeifTrueRule(SurfaceRules.ON_FLOOR, makeStateRule(Blocks.GRAVEL)), makeifTrueRule(SurfaceRules.UNDER_FLOOR, makeStateRule(Blocks.GRAVEL)));
 
 
-    private static final SurfaceRules.RuleSource PURPLE_SAND = makeSandRule(BWGBlocks.PURPLE_SAND_SET);
-    private static final SurfaceRules.RuleSource BLUE_SAND = makeSandRule(BWGBlocks.BLUE_SAND_SET);
-    private static final SurfaceRules.RuleSource PINK_SAND = makeSandRule(BWGBlocks.PINK_SAND_SET);
-    private static final SurfaceRules.RuleSource BLACK_SAND = makeSandRule(BWGBlocks.BLACK_SAND_SET);
-    private static final SurfaceRules.RuleSource WHITE_SAND = makeSandRule(BWGBlocks.WHITE_SAND_SET);
+    private static final SurfaceRules.RuleSource PURPLE_SAND = makeBeachSandRule(BWGBlocks.PURPLE_SAND_SET);
+    private static final SurfaceRules.RuleSource BLUE_SAND = makeBeachSandRule(BWGBlocks.BLUE_SAND_SET);
+    private static final SurfaceRules.RuleSource PINK_SAND = makeBeachSandRule(BWGBlocks.PINK_SAND_SET);
+    private static final SurfaceRules.RuleSource BLACK_SAND = makeBeachSandRule(BWGBlocks.BLACK_SAND_SET);
+    private static final SurfaceRules.RuleSource WHITE_SAND = makeBeachSandRule(BWGBlocks.WHITE_SAND_SET);
 
 
     private static final SurfaceRules.RuleSource WHITE_OVERGROWN_DACITE_DACITE_SURFACE = SurfaceRules.sequence(makeifTrueRule(WATER_CHECK, makeifTrueRule(SurfaceRules.ON_FLOOR, BWGBlocks.WHITE_OVERGROWN_DACITE.get())), makeifTrueRule(SurfaceRules.UNDER_FLOOR, BWGBlocks.WHITE_DACITE_SET.getBase()));
@@ -417,7 +417,13 @@ public class BWGOverworldSurfaceRules {
     private static final SurfaceRules.RuleSource BLACK_ICE_BANDS = biomeAbovePreliminarySurface(BWGBiomes.SHATTERED_GLACIER, new BandsRuleSource(WeightedList.<BlockState>builder().add(BWGBlocks.PACKED_BLACK_ICE.get().defaultBlockState(), 3).add(BWGBlocks.BLACK_ICE.get().defaultBlockState(), 1).build(), UniformInt.of(1, 5), UniformInt.of(20, 40), 1, 10));
 //    public static final SurfaceRules.RuleSource BOREALIS_ICE_BANDS = biomeAbovePreliminarySurface(BWGBiomes.ERODED_BOREALIS, new BandsRuleSource(SimpleWeightedRandomList.<BlockState>builder().add(BWGBlocks.PACKED_BOREALIS_ICE.get().defaultBlockState(), 3).add(BWGBlocks.BOREALIS_ICE.get().defaultBlockState(), 1).build(), UniformInt.of(1, 5), UniformInt.of(20, 40), 1, 10));
 
-    private static final SurfaceRules.RuleSource WINDSWEPT_DESERT = biomeAbovePreliminarySurface(BWGBiomes.WINDSWEPT_DESERT, makeSandRule(BWGBlocks.WINDSWEPT_SAND_SET));
+    private static final SurfaceRules.RuleSource WINDSWEPT_DESERT = makeifTrueRule(BWGBiomes.WINDSWEPT_DESERT, SurfaceRules.sequence(
+            SurfaceRules.sequence(
+                    makeifTrueRule(SurfaceRules.ON_FLOOR, makeStateRule(BWGBlocks.WINDSWEPT_SAND_SET.getSand())),
+                    makeifTrueRule(SurfaceRules.UNDER_FLOOR, makeStateRule(BWGBlocks.WINDSWEPT_SAND_SET.getSand())),
+                    makeifTrueRule(SurfaceRules.VERY_DEEP_UNDER_FLOOR, makeStateRule(BWGBlocks.WINDSWEPT_SAND_SET.getSandstone()))
+            )
+    ));
 
     /**
      * Makes the surface rules for the overworld biomes in Oh The Biomes We've Gone.
@@ -472,7 +478,7 @@ public class BWGOverworldSurfaceRules {
     }
 
     // Helper methods
-    private static SurfaceRules.RuleSource makeSandRule(@NotNull BWGSandSet sandSet) {
+    private static SurfaceRules.RuleSource makeBeachSandRule(@NotNull BWGSandSet sandSet) {
         return SurfaceRules.sequence(
                 makeifTrueRule(SurfaceRules.ON_FLOOR, sandSet.getSand()),
                 makeifTrueRule(SurfaceRules.UNDER_FLOOR, sandSet.getSand()),
