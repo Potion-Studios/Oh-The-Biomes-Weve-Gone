ecde4df68c5d435a1cb458486911bd58e1527b44
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-06-13 12:06:35 -0400

    Move from CorgiLib to TYG for RandomTickScheduler
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/feature/MediumPumpkinFeature.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/feature/MediumPumpkinFeature.java
@@ -1,7 +1,7 @@
 package net.potionstudios.biomeswevegone.world.level.levelgen.feature;
 
 import com.mojang.serialization.Codec;
-import corgitaco.corgilib.world.level.RandomTickScheduler;
+import dev.corgitaco.ohthetreesyoullgrow.world.level.chunk.RandomTickScheduler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
