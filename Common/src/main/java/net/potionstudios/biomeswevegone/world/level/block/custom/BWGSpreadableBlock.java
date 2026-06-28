288894c83f84955593b951fad8101b6878bb706a
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-06-27 22:31:32 -0400

    Fix Spreadable Block

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/custom/BWGSpreadableBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/custom/BWGSpreadableBlock.java
@@ -22,16 +22,16 @@ public class BWGSpreadableBlock extends GrassBlock implements BonemealableBlock 
 
 	@Override
 	public void randomTick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
-//		if (!SnowyBlock.canBeGrass(state, level, pos))
-//			level.setBlockAndUpdate(pos, this.spreadable.get().defaultBlockState());
-//		else if (level.getMaxLocalRawBrightness(pos.above()) >= 9) {
-//			BlockState blockstate = this.defaultBlockState();
-//			for (int i = 0; i < 4; ++i) {
-//				BlockPos blockpos = pos.offset(random.nextInt(3) - 1, random.nextInt(5) - 3, random.nextInt(3) - 1);
-//				if (level.getBlockState(blockpos).is(this.spreadable.get()) && SnowyBlock.canPropagate(blockstate, level, blockpos))
-//					level.setBlockAndUpdate(blockpos, blockstate.setValue(SNOWY, level.getBlockState(blockpos.above()).is(Blocks.SNOW)));
-//			}
-//		}
+		if (!SpreadingSnowyBlock.canStayAlive(state, level, pos))
+			level.setBlockAndUpdate(pos, this.spreadable.get().defaultBlockState());
+		else if (level.getMaxLocalRawBrightness(pos.above()) >= 9) {
+			BlockState blockstate = this.defaultBlockState();
+			for (int i = 0; i < 4; ++i) {
+				BlockPos blockpos = pos.offset(random.nextInt(3) - 1, random.nextInt(5) - 3, random.nextInt(3) - 1);
+				if (level.getBlockState(blockpos).is(this.spreadable.get()) && SpreadingSnowyBlock.canPropagate(blockstate, level, blockpos))
+					level.setBlockAndUpdate(blockpos, blockstate.setValue(SNOWY, level.getBlockState(blockpos.above()).is(Blocks.SNOW)));
+			}
+		}
 	}
 
 	@Override
